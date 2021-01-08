244c3dee16c9254c079277ee4a84e08eb8c51271
Ubuntu <ubuntu@ip-172-31-3-61.ap-southeast-1.compute.internal>
2021-01-08 23:53:01 +0000

    hello

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,34 +1,23 @@
-require('dotenv').config()
-
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_CONNECTION,
-    operatorsAliases: process.env.DB_OPERATOR_ALIAS,
-    timezone: 'Africa/Addis_Ababa',
+{
+  "development": {
+    "username": "root",
+    "password": "pass",
+    "database": "example",
+    "host": "10.10.1.120",
+    "dialect": "mysql"
   },
-  staging: {
-    username: process.env.STAGING_DB_USERNAME,
-    password: process.env.STAGING_DB_PASSWORD,
-    database: process.env.STAGING_DB_DATABASE,
-    host: process.env.STAGING_DB_HOST,
-    port: process.env.STAGING_DB_PORT,
-    dialect: process.env.STAGING_DB_CONNECTION,
-    operatorsAliases: process.env.STAGING_DB_OPERATOR_ALIAS,
-    timezone: process.env.STAGING_DB_TIMEZONE,
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_DATABASE,
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    dialect: process.env.PROD_DB_CONNECTION,
-    operatorsAliases: process.env.PROD_DB_OPERATOR_ALIAS,
-    timezone: process.env.PROD_DB_TIMEZONE,
-  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
 }
